40f693700d029d0ba34253045e88b6bffca8853d
marina <marinasafwat883@gmail.com>
2025-11-17 13:32:58 -0600

    addition changes for login

--- smp/lib/main.dart
+++ smp/lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:smp/firebase_options.dart';
 import 'package:smp/screens/login/login_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: LoginScreen(),
     ),
   );
--- smp/lib/screens/login/home_screen.dart
+++ smp/lib/screens/login/home_screen.dart
@@ -15,7 +15,14 @@ class _HomeScreenState extends State<HomeScreen> {
 
   void logout() async {
     try {
-      await authService.value.signOut();
+      //await authService.value.signOut();
+      await authService.value.googleSignOut();
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const LoginScreen(),
+        ),
+      );
     } on FirebaseAuthException catch (e) {
       setState(() {
         errorMessage = e.message ?? "An unknown error occurred.";
@@ -29,6 +36,8 @@ class _HomeScreenState extends State<HomeScreen> {
       body: Center(
         child: Column(
           children: [
+            Text("${FirebaseAuth.instance.currentUser!.displayName}"),
+            Text("${FirebaseAuth.instance.currentUser!.email}"),
             Text(
               errorMessage,
               style: const TextStyle(color: Colors.redAccent),
@@ -38,12 +47,7 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
             ElevatedButton(
                 onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => LoginScreen(),
-                    ),
-                  );
+                  logout();
                 },
                 child: const Text('Log Out')),
           ],
